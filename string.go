6b4bbb046707cdb9c060c516a2c09fb316db32d2
Agung <agung.suprayitno92@hotmail.com>
2022-06-04 13:38:09 +0700

    Data type string.

---
+++ string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Agung")
+	fmt.Println("Agung Suprayitno")
+	fmt.Println("Panjang String=", len("Agung Suprayitno"))
+	fmt.Println("Character=", "Agung Suprayitno"[2])
+}
