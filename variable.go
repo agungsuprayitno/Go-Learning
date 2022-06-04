0a887171c5fd772e08aec642f0db1dbcd9f4c4b1
Agung <agung.suprayitno92@hotmail.com>
2022-06-04 13:50:41 +0700

    Variable.

---
+++ variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Agung Suprayitno"
+	fmt.Println(name)
+
+	name = "Agung Suprayitno2"
+	fmt.Println(name)
+
+	var mySonName = "Satria"
+	fmt.Println(mySonName)
+
+	var age int8 = 30
+	fmt.Println(age)
+
+	myFriendName := "Agung"
+	fmt.Println(myFriendName)
+
+	var (
+		firstName = "Agung"
+		lastName  = "Suparyitno"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
