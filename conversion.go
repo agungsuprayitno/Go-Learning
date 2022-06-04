4988549d02ec2509a5bbfa13175bf068d2cb6ed8
Agung <agung.suprayitno92@hotmail.com>
2022-06-04 14:52:23 +0700

    Conversion data type.

---
+++ conversion.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai32 int32 = 10000
+	var nilai64 int64 = int64(nilai32)
+	var nilai16 int16 = int16(nilai32)
+
+	fmt.Println(nilai32)
+	fmt.Println(nilai64)
+	fmt.Println(nilai16)
+
+	name := "Agung"
+	var a byte = name[0]
+	aString := string(a)
+
+	println(aString)
+}
